3afa72244c81d9f5d13997df36f0255d6d3038a1
lizcalixto <calixtolizmex@gmail.com>
2018-05-02 15:08:19 -0500

    2da clase java script

--- JavaScript/javaScript3.js
+++ JavaScript/javaScript3.js
@@ -1,4 +1,10 @@
 
-function javaNoEsJavascrit() {
-document.getElementById("responsabilidad").innerHTML = "Javascript no es Java.";
+function resultado() {
+    var text = "";
+    var i;
+    for (i = 1; i < 11; i++) {
+        text += i + "<br>";
+    }
+    document.getElementById("resultado").innerHTML = text;  
+
 }
